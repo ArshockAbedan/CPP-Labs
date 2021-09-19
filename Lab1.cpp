2f7ce231f8e538a9995215ad943eddc1894e438e
Aref Abedjooy <arefabedjooy@gmail.com>
2021-09-19 18:11:21 -0400

    Add Top Message

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,3 +1,12 @@
+//============================================================================
+// Name        : Lab1.cpp
+// Author      : Aref Abedjooy
+// Version     : 1.0
+// Copyright   : Your copyright notice
+// Description : This is a sample code for Lab-01
+//               in the "CSCI 1060U – Programming Workshop" course 
+//               which was taught at Ontario tech University during Fall 2021 term. 
+//============================================================================
 #include <iostream>
 #include <ctime>
 using namespace std;
